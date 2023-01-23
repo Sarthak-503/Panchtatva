65ead6253ade18d1e3ca252b196fba62e395e3fe
Pranav Verma <vermapranav3827@gmail.com>
2023-01-23 10:53:07 +0530

    new commit

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,12 @@ import express from 'express';
 import data from './data.js';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import seedRouter from './routes/seedRoutes.js';
+import productRouter from './routes/productRoutes.js';
 
 dotenv.config();
+
+mongoose.set('strictQuery', false);
 
 mongoose
 .connect(process.env.MONGODB_URI)
@@ -15,36 +19,8 @@ mongoose
 });
 
 const app = express();
-
-app.get('/api/products', (req, res) => {
-    res.send(data.products);
-});
-
-app.get('/api/products/slug/:slug', (req, res) => {
-    const product = data.products.find((x) => x.slug === req.params.slug);
-    if(product)
-    {
-        res.send(product);  
-    }
-
-    else
-    {
-        res.status(404).send({message: 'Product not found'});
-    }
-});
-
-app.get('/api/products/:id', (req, res) => {
-    const product = data.products.find((x) => x._id === req.params.id);
-    if(product)
-    {
-        res.send(product);
-    }  
-
-    else
-    {
-        res.status(404).send({message: 'Product not found'});
-    }
-});
+app.use('/api/seed', seedRouter);
+app.use('/api/products', productRouter);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
